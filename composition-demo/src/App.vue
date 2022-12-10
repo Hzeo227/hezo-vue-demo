5dffa9a1d433a5fc32c7387fe8e171169ab42048
Hzeo227 <zxveren@gmail.com>
2022-12-10 13:17:49 +0800

    ref() shallow deep

--- composition-demo/src/App.vue
+++ composition-demo/src/App.vue
@@ -1,10 +1,38 @@
 <template>
-
+  <h1>counter:{{counter}}</h1>
+  <button @click="increase">+1</button>
+  <button @click="counter++">+1</button>
+  <br>
+  <h1>info: {{info.counter}}</h1>
+  <button @click="info.counter.value++">+1</button>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 
 export default {
+  setup() {
+    let counter = ref(0)
+    const increase = () => {
+      counter.value++
+      console.log(counter)
+    }
+
+    let info = {
+      counter
+    }
+    const increaseInfo = () => {
+      info.counter.value++
+    }
+
+    return {
+      counter,
+      increase,
+      info,
+      increaseInfo
+    }
+  }
 }
 </script>
 
