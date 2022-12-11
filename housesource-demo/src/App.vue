760ab61c9c7eaaa9c2617f03878b2daa7e761f34
Hzeo227 <zxveren@gmail.com>
2022-12-11 12:25:33 +0800

    housesource-demo done

--- housesource-demo/src/App.vue
+++ housesource-demo/src/App.vue
@@ -1,26 +1,22 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import AreaRoom from './components/AreaRoom.vue'
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+import highScore from './data/high_score.json'
+console.log(highScore)
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<template>
+  <div class="app">
+    <area-room :area-data="highScore" />
+  </div>
+
+</template>
+
+<style lang="less" scoped>
+.app {
+  width: 1032px;
+  padding: 48px;
+  margin: 0 auto;
 }
 </style>
---
+++ housesource-demo/src/components/AreaRoom.vue
@@ -0,0 +1,34 @@
+<script setup>
+import AreaHeader from './AreaHeader.vue'
+import RoomItem from './RoomItem.vue'
+
+defineProps({
+  areaData: {
+    type: Object,
+    default: () => ({})
+  }
+})
+</script>
+
+<template>
+  <div class="area-room">
+    <!-- Header -->
+    <area-header  :title="areaData.title" :subtitle="areaData.subtitle"></area-header>
+
+    <!-- High Score House Source -->
+
+    <div class="room-list">
+    <template v-for="item in areaData.list" :key="item.id">
+      <room-item :high-score-list="item"></room-item>
+    </template>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+.room-list {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 20px -8px;
+}
+</style>
